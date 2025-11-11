be4867a759d73055955b58d688dd532245a5d7c1
Hao Wu <hwu27@gmu.edu>
2025-11-11 04:00:25 +0000

    add matmul_ogs to kernels

--- fig15/run_exp_and_visualize.sh
+++ fig15/run_exp_and_visualize.sh
@@ -25,6 +25,7 @@ fi
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/topk_original.py --profile --buffer-size 2048 --use-cuda-event
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/persistent_matmul.py --profile --buffer-size 512 --use-cuda-event
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/fused_attention.py --profile --buffer-size 1024 --simple --use-cuda-event
+    TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/matmul_ogs_original.py --profile --buffer-size 256 --use-cuda-event
 
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/fused_softmax.py --profile --instrument --buffer-size 1024 --use-cuda-event
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/grouped_gemm_original.py --profile --instrument --buffer-size 1024 --use-cuda-event
@@ -35,6 +36,7 @@ fi
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/topk_original.py --profile --instrument --buffer-size 2048 --use-cuda-event
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/persistent_matmul.py --profile --instrument --buffer-size 512 --use-cuda-event
     TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/fused_attention.py --profile  --instrument --buffer-size 1024 --simple --use-cuda-event
+    TRITON_ALWAYS_COMPILE=1 TRITON_KERNEL_DUMP=1 TRITON_DUMP_DIR="$DUMP_DIR" python kernels/matmul_ogs_original.py --profile --instrument --buffer-size 256 --use-cuda-event
     # TODO (Hao): visualize results and produce the figure
 
 )

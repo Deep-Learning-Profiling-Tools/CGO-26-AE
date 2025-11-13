e25a1d66d022c99167a9fb88b05057cf671db12a
Jokeren <robinho364@gmail.com>
2025-11-13 10:35:08 -0500

    [DEV] Fix

--- fig10/run.sh
+++ fig10/run.sh
@@ -2,7 +2,7 @@
 
 # It only works for rocm 6.4
 
-cd triton_kernels/bench_mlp
+cd triton_kernels/bench
 
 torchrun --nproc-per-node=8 ./bench_mlp.py --tp 1 --ep 8 --name gpt-oss-x2
 torchrun --nproc-per-node=8 ./bench_mlp.py --tp 2 --ep 4 --name gpt-oss-x2
